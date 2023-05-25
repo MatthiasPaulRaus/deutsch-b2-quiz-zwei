e62bd8c31a1bf09df802fc2e3a0573b1dfc83fdf
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-05-25 19:10:39 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Viele Studenten machen schon ein Praktikum,",
+        answersList: [
+            {answer:"während sie noch studieren.", isCorrect:true},
+            {answer:"solange sie noch zur Schule gehen.", isCorrect:false},
+            {answer:"nach dem Studienabschluss.", isCorrect:false},
+        ]
+    },{
+        question:"Sobald ich mein Studium beendet habe,",
+        answersList: [
+            {answer:"habe ich eine Arbeit gesucht.", isCorrect:false},
+            {answer:"werde ich eine Arbeit finden.", isCorrect:true},
+            {answer:"hätte ich eine Stelle gefunden.", isCorrect:false},
+        ]
+    },{
+        question:"Ehe ich eine Ausbildung mache,",
+        answersList: [
+            {answer:"musste ich ein Praktikum absolvieren.", isCorrect:false},
+            {answer:"kann ich keine Reise ins Ausland machen.", isCorrect:false},
+            {answer:"möchte ich eine Auslandsreise machen.", isCorrect:true},
+        ]
+    },{
+        question:"Nachdem Sie Ihr Studium abgeschlossen haben,",
+        answersList: [
+            {answer:"können Sie einen Master-Studiengang wählen.", isCorrect:true},
+            {answer:"können Sie keinen Master-Studiengang mehr wählen.", isCorrect:false},
+            {answer:"mussten Sie einen Master-Studiengang absolvieren. ", isCorrect:false},
+        ]
+    },{
+        question:"Sobald du dein Abitur gemacht hast,",
+        answersList: [
+            {answer:"konntest du studieren.", isCorrect:false},
+            {answer:"kannst du die Universität besuchen.", isCorrect:true},
+            {answer:"musst du den Schulabschluss machen.", isCorrect:false},
+        ]
+    },{
+        question:"Bevor Sie sich im Ausland bewerben,",
+        answersList: [
+            {answer:"recherchieren Sie die Bewerbungsunterlagen.", isCorrect:false},
+            {answer:"brauchen Sie ein Visum für Touristen.", isCorrect:false},
+            {answer:"müssen Sie eine Arbeitserlaubnis beantragen.", isCorrect:true},
+        ]
+    },{
+        question:"Als ich meine Bewerbung schreiben wollte,",
+        answersList: [
+            {answer:"ging mein Computer kaputt.", isCorrect:true},
+            {answer:"hatte ich schon einen Computer.", isCorrect:false},
+            {answer:"wurde mein Computer gespeichert.", isCorrect:false},
+        ]
+    },{
+        question:"Was du da behauptest,",
+        answersList: [
+            {answer:"hatte weder Kopf noch Bein.", isCorrect:false},
+            {answer:"hat weder Hand noch Fuß.", isCorrect:true},
+            {answer:"hat weder Hals noch Kopf.", isCorrect:false},
+        ]
+    },{
+        question:"Ich würde dir auf jeden Fall empfehlen,",
+        answersList: [
+            {answer:"Informationen vor dem Praktikum suchen.", isCorrect:false},
+            {answer:"dich vor dem Praktikum informieren.", isCorrect:false},
+            {answer:"schon vor dem Praktikum Informationen zu suchen.", isCorrect:true},
+        ]
+    },{
+        question:"Sie sollten unbedingt vor der Operation",
+        answersList: [
+            {answer:"eine Untersuchung machen lassen.", isCorrect:true},
+            {answer:"etwas untersuchen lassen.", isCorrect:false},
+            {answer:"eine Analyse untersuchen lassen.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Für uns persönlich kommt das nicht infrage,",
+        answersList: [
+            {answer:"denn wir sehen das sehr kritisch.", isCorrect:true},
+            {answer:"denn wir sind sehr optimistisch.", isCorrect:false},
+            {answer:"weil für voll und ganz dafür sind.", isCorrect:false},
+        ]
+    },{
+        question:"Was haben Sie in Ihrer Freizeit gemacht?",
+        answersList: [
+            {answer:"Manche wollen surfen lernen.", isCorrect:false},
+            {answer:"Wir haben tanzen gelernt.", isCorrect:true},
+            {answer:"Ganz viele Leute könnten tanzen lernen.", isCorrect:false},
+        ]
+    },{
+        question:"Ich habe den Nachbarn laut singen hören.",
+        answersList: [
+            {answer:"Wirklich? Ich habe ihn nicht singen gehört?", isCorrect:false},
+            {answer:"Echt? Das könnte ich mir nicht vorstellen.", isCorrect:false},
+            {answer:"Das kann ich mir gut vorstellen. Er ist immer gut gelaunt.", isCorrect:true},
+        ]
+    },{
+        question:"Deine Kollegen werden neidisch gewesen sein.",
+        answersList: [
+            {answer:"Das kannst du gerne glauben.", isCorrect:true},
+            {answer:"Meine Kollegen werden es nicht glauben können.", isCorrect:false},
+            {answer:"Sie können es sich nicht erklären.", isCorrect:false},
+        ]
+    },{
+        question:"Deine Eltern waren vermutlich ziemlich enttäuscht.",
+        answersList: [
+            {answer:"Na ja, sie wären nicht unzufrieden gewesen.", isCorrect:false},
+            {answer:"Ja, aber sie haben meine Entscheidung akzeptiert.", isCorrect:true},
+            {answer:"Da hast du Recht. Sie vermuten es schon.", isCorrect:false},
+        ]
+    },{
+        question:"Bist du zufrieden mit der neuen Wohnung?",
+        answersList: [
+            {answer:"Klar, wir haben sie frisch renoviert verlassen.", isCorrect:false},
+            {answer:"Hm, der Vermieter würde sie nicht streichen lassen.", isCorrect:false},
+            {answer:"Ja, und ich habe sie gelassen, wie sie war.", isCorrect:true},
+        ]
+    },{
+        question:"Die Leiterin wird nicht zulassen,",
+        answersList: [
+            {answer:"dass ihr alleine nachts ausgeht.", isCorrect:true},
+            {answer:"wenn ihr zusammen ausgeht.", isCorrect:false},
+            {answer:"weil ihr in Begleitung ausgeht.", isCorrect:false},
+        ]
+    },{
+        question:"Es ist wirklich erstaunlich,",
+        answersList: [
+            {answer:"was man mit dem Gerät machen lässt.", isCorrect:false},
+            {answer:"was sich mit dem Gerät alles machen lässt.", isCorrect:true},
+            {answer:"was man damit alles machen lässt.", isCorrect:false},
+        ]
+    },{
+        question:"Sie können es einfach nicht sein lassen.",
+        answersList: [
+            {answer:"Sie haben schon genug davon.", isCorrect:false},
+            {answer:"Wir sind ziemlich bescheiden.", isCorrect:false},
+            {answer:"Wir können nicht genug davon bekommen.", isCorrect:true},
+        ]
+    },{
+        question:"Da wirst du wohl sehr gestaunt haben.",
+        answersList: [
+            {answer:"Ja, es war unglaublich.", isCorrect:true},
+            {answer:"Ja, wir werden uns wundern.", isCorrect:false},
+            {answer:"Na ja, ich kann es nicht glauben.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Wenn du damals nicht programmieren gelernt hättest,",
+        answersList: [
+            {answer:"könntest du jetzt keine Apps entwickeln.", isCorrect:true},
+            {answer:"kannst du heutzutage alles entwickeln.", isCorrect:false},
+            {answer:"würdest du heute deine eigenen Apps entwickeln.", isCorrect:false},
+        ]
+    },{
+        question:"Hätten Sie nicht ein Start-up gegründet",
+        answersList: [
+            {answer:"würden Sie heute ein Gründer sein.", isCorrect:false},
+            {answer:"wären Sie heute nicht selbständig.", isCorrect:true},
+            {answer:"hätten Sie heutzutage kaum Möglichkeiten.", isCorrect:false},
+        ]
+    },{
+        question:"Wovon waren die Teilnehmer begeistert?",
+        answersList: [
+            {answer:"Für die professionellen Moderatoren.", isCorrect:false},
+            {answer:"Auf die professionelle Darbietung.", isCorrect:false},
+            {answer:"Von der professionellen Moderation.", isCorrect:true},
+        ]
+    },{
+        question:"Bei wem ist der Abendspaziergang besonders beliebt?",
+        answersList: [
+            {answer:"Bei den älteren Kursteilnehmenden.", isCorrect:true},
+            {answer:"Die ältesten nehmen nicht daran teil.", isCorrect:false},
+            {answer:"Alle wollen morgen daran teilnehmen.", isCorrect:false},
+        ]
+    },{
+        question:"Der Besuch des Freizeitparks ist",
+        answersList: [
+            {answer:"für die Teilnehmer wohl nicht freiwillig.", isCorrect:false},
+            {answer:"wahrscheinlich für die Kunden zu billig.", isCorrect:false},
+            {answer:"wohl eher für die Jugendlichen geeignet.", isCorrect:true},
+        ]
+    },{
+        question:"Alles ist trocken, aber es sieht so aus,",
+        answersList: [
+            {answer:"als ob es doch noch geregnet hätte.", isCorrect:true},
+            {answer:"als ob es regnen konnte.", isCorrect:false},
+            {answer:"als ob es hätte regnen sollen.", isCorrect:false},
+        ]
+    },{
+        question:"Wären Sie doch lieber im Hotel geblieben.",
+        answersList: [
+            {answer:"Dann hätte es nicht so stark geregnet.", isCorrect:false},
+            {answer:"Dann wären Sie nicht in den Regen gekommen.", isCorrect:true},
+            {answer:"Dann wären Sie sicher nass geworden.", isCorrect:false},
+        ]
+    },{
+        question:"Wir sind begeistert von der kulturellen Vielfalt der Stadt.",
+        answersList: [
+            {answer:"Ja, die Stadt ist begeistert.", isCorrect:false},
+            {answer:"Da bin ich ganz meiner Meinung.", isCorrect:false},
+            {answer:"Dem kann ich mich nur anschließen.", isCorrect:true},
+        ]
+    },{
+        question:"Kostenlose öffentliche Verkehrsmittel",
+        answersList: [
+            {answer:"könnten eine Lösung des Problems sein.", isCorrect:true},
+            {answer:"sind für den Verkehr problematisch.", isCorrect:false},
+            {answer:"sind für die Verbraucher ein Kundenservice.", isCorrect:false},
+        ]
+    },{
+        question:"Erlauben Sie mir,",
+        answersList: [
+            {answer:"zusammenzufassen: Ich werde beeindruckt.", isCorrect:false},
+            {answer:"es auf den Punkt zu bringen: Ich bin beeindruckend.", isCorrect:false},
+            {answer:"Bilanz zu ziehen: Es war sehr beeindruckend.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
